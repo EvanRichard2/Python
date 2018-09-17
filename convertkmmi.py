4ef1265f1418d0ba564d019c531a87b8cf81c25d
Lelouch Lamperouge <31546491+EvanRichard2@users.noreply.github.com>
2018-09-17 08:37:19 -0700

    Update convertkmmi.py

--- convertkmmi.py
+++ convertkmmi.py
@@ -1,27 +1,43 @@
 def print_menu():
 	print('1. Kilometers to Miles')
 	print('2. Miles to Kilometers')
-	
+
 def km_miles():
 	km = float(input('Enter distance in kilometers: '))
 	miles = km / 1.609
-	
+
 	print('Distance in miles: {0}'.format(miles))
-	
+
 def miles_km():
 	miles = float(input('Enter distance in miles: '))
 	km = miles * 1.609
-	
+
 	print('Distance in kilometers: {0}'.format(km))
-	
+
 if __name__ == '__main__':
 	print_menu()
-	choice - input('Which conversion would you like to do?: ')
-	if choice == '1":
+	choice = input('Which conversion would you like to do?: ')
+	if choice == '1':
 		km_miles()
-	
-	if choice == '2':
-		miles_km()
-	
-	
-	
+
+if choice == '2':
+    miles_km()
+
+
+'''
+console output
+darkainian@romeo:~/python$ python3 convert+kmmi.py
+1. Kilometers to Miles
+2. Miles to Kilometers
+Which conversion would you like to do?: 1
+Enter distance in kilometers: 5
+Distance in miles: 3.107520198881293
+darkainian@romeo:~/python$ python3 convert+kmmi.py
+1. Kilometers to Miles
+2. Miles to Kilometers
+Which conversion would you like to do?: 2
+Enter distance in miles: 5
+Distance in kilometers: 8.045
+darkainian@romeo:~python$
+
+'''
