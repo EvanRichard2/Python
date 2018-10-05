0389740f3381b968a0594df09770de065aefeb99
Lelouch Lamperouge <31546491+EvanRichard2@users.noreply.github.com>
2018-10-05 10:41:41 -0500

    Add files via upload

---
+++ Cyber_18/types1.py
@@ -0,0 +1,12 @@
+# types1.py darkainian
+x = -100000
+y = 2.71828
+z = 3+1j
+s = "A"
+#print the variable values
+print(x,y,z,s)
+#print types
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(s))
